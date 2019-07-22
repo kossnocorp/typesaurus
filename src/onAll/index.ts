190f8fbd1c6f50ba1005ead735692f05c4f8213d
Sasha Koss <koss@nocorp.me>
2019-07-22 13:10:10 +0400

    Add onError handlers to onAll and onGet

--- src/onAll/index.ts
+++ src/onAll/index.ts
@@ -6,12 +6,13 @@ import { wrapData } from '../data'
 
 export default function onAll<Model>(
   collection: Collection<Model>,
-  onResult: (docs: Doc<Model>[]) => any
+  onResult: (docs: Doc<Model>[]) => any,
+  onError?: (error: Error) => any
 ): () => void {
   return firestore.collection(collection.path).onSnapshot(firebaseSnap => {
     const docs = firebaseSnap.docs.map(d =>
       doc(ref(collection, d.id), wrapData(d.data()) as Model)
     )
     onResult(docs)
-  })
+  }, onError)
 }
--- src/onGet/index.ts
+++ src/onGet/index.ts
@@ -6,32 +6,43 @@ import { wrapData } from '../data'
 
 type OnResult<Model> = (doc: Doc<Model> | undefined) => any
 
-function onGet<Model>(ref: Ref<Model>, onResult: OnResult<Model>): () => void
+type OnError = (error: Error) => any
+
+function onGet<Model>(
+  ref: Ref<Model>,
+  onResult: OnResult<Model>,
+  onError?: OnError
+): () => void
 
 function onGet<Model>(
   collection: Collection<Model>,
   id: string,
-  onResult: OnResult<Model>
+  onResult: OnResult<Model>,
+  onError?: OnError
 ): () => void
 
 function onGet<Model>(
   collectionOrRef: Collection<Model> | Ref<Model>,
   idOrOnResult: string | OnResult<Model>,
-  maybeOnResult?: OnResult<Model>
+  onResultOrOnError?: OnResult<Model> | OnError,
+  maybeOnError?: OnError
 ): () => void {
   let collection: Collection<Model>
   let id: string
   let onResult: OnResult<Model>
+  let onError: OnError | undefined
 
   if (collectionOrRef.__type__ === 'collection') {
     collection = collectionOrRef as Collection<Model>
     id = idOrOnResult as string
-    onResult = maybeOnResult as OnResult<Model>
+    onResult = onResultOrOnError as OnResult<Model>
+    onError = maybeOnError
   } else {
     const ref = collectionOrRef as Ref<Model>
     collection = ref.collection
     id = ref.id
     onResult = idOrOnResult as OnResult<Model>
+    onError = onResultOrOnError as OnError | undefined
   }
 
   const firestoreDoc = firestore.collection(collection.path).doc(id)
@@ -39,7 +50,7 @@ function onGet<Model>(
     const firestoreData = firestoreSnap.data()
     const data = firestoreData && (wrapData(firestoreData) as Model)
     onResult(data && doc(ref(collection, id), data))
-  })
+  }, onError)
 }
 
 export default onGet
