6e2a5ffe80a6078049300a3b55f4dfa05e75c08f
Sasha Koss <koss@nocorp.me>
2019-07-25 10:18:16 +0200

    Add batch operations

---
+++ src/batch/index.ts
@@ -0,0 +1,150 @@
+import firestore from '../adaptor'
+import { Collection } from '../collection'
+import { Ref, ref } from '../ref'
+import { Doc, doc } from '../doc'
+import { unwrapData } from '../data'
+import set from '../set'
+import update, { ModelUpdate } from '../update'
+import { Field } from '../field'
+import clear from '../clear'
+
+export type BatchAPI = {
+  set: typeof set
+  update: typeof update
+  clear: typeof clear
+  commit: () => Promise<void>
+}
+
+export default function batch() {
+  const b = firestore.batch()
+
+  // set
+
+  function set<Model>(ref: Ref<Model>, data: Model): Doc<Model>
+
+  function set<Model>(
+    collection: Collection<Model>,
+    id: string,
+    data: Model
+  ): Doc<Model>
+
+  function set<Model>(
+    collectionOrRef: Collection<Model> | Ref<Model>,
+    idOrData: string | Model,
+    maybeData?: Model
+  ): Doc<Model> {
+    let collection: Collection<Model>
+    let id: string
+    let data: Model
+
+    if (collectionOrRef.__type__ === 'collection') {
+      collection = collectionOrRef as Collection<Model>
+      id = idOrData as string
+      data = maybeData as Model
+    } else {
+      const ref = collectionOrRef as Ref<Model>
+      collection = ref.collection
+      id = ref.id
+      data = idOrData as Model
+    }
+
+    const firestoreDoc = firestore.collection(collection.path).doc(id)
+    // ^ above
+    // TODO: Refactor code above and below because is all the same as in the regular set function
+    b.set(firestoreDoc, unwrapData(data))
+    // v below
+    return doc(ref(collection, id), data)
+  }
+
+  // update
+
+  function update<Model>(
+    collection: Collection<Model>,
+    id: string,
+    data: Field<Model>[]
+  ): void
+
+  function update<Model>(ref: Ref<Model>, data: Field<Model>[]): void
+
+  function update<Model>(
+    collection: Collection<Model>,
+    id: string,
+    data: ModelUpdate<Model>
+  ): void
+
+  function update<Model>(ref: Ref<Model>, data: ModelUpdate<Model>): void
+
+  function update<Model>(
+    collectionOrRef: Collection<Model> | Ref<Model>,
+    idOrData: string | Field<Model>[] | ModelUpdate<Model>,
+    maybeData?: Field<Model>[] | ModelUpdate<Model>
+  ): void {
+    let collection: Collection<Model>
+    let id: string
+    let data: Model
+
+    if (collectionOrRef.__type__ === 'collection') {
+      collection = collectionOrRef as Collection<Model>
+      id = idOrData as string
+      data = maybeData as Model
+    } else {
+      const ref = collectionOrRef as Ref<Model>
+      collection = ref.collection
+      id = ref.id
+      data = idOrData as Model
+    }
+
+    const firebaseDoc = firestore.collection(collection.path).doc(id)
+    const updateData = Array.isArray(data)
+      ? data.reduce(
+          (acc, { key, value }) => {
+            acc[Array.isArray(key) ? key.join('.') : key] = value
+            return acc
+          },
+          {} as { [key: string]: any }
+        )
+      : data
+    // ^ above
+    // TODO: Refactor code above because is all the same as in the regular update function
+    b.update(firebaseDoc, unwrapData(updateData))
+  }
+
+  // clear
+
+  function clear<Model>(collection: Collection<Model>, id: string): void
+
+  function clear<Model>(ref: Ref<Model>): void
+
+  function clear<Model>(
+    collectionOrRef: Collection<Model> | Ref<Model>,
+    maybeId?: string
+  ): void {
+    let collection: Collection<Model>
+    let id: string
+
+    if (collectionOrRef.__type__ === 'collection') {
+      collection = collectionOrRef as Collection<Model>
+      id = maybeId as string
+    } else {
+      const ref = collectionOrRef as Ref<Model>
+      collection = ref.collection
+      id = ref.id
+    }
+
+    const firebaseDoc = firestore.collection(collection.path).doc(id)
+    // ^ above
+    // TODO: Refactor code above because is all the same as in the regular update function
+    b.delete(firebaseDoc)
+  }
+
+  async function commit() {
+    await b.commit()
+  }
+
+  return {
+    set,
+    update,
+    clear,
+    commit
+  }
+}
---
+++ src/batch/test.ts
@@ -0,0 +1,82 @@
+import assert from 'assert'
+import batch from '.'
+import { collection } from '../collection'
+import { ref } from '../ref'
+import nanoid from 'nanoid'
+import get from '../get'
+import set from '../set'
+
+describe('batch', () => {
+  type User = { name: string }
+  const users = collection<User>('users')
+
+  it('performs batch operations', async () => {
+    const { set, commit } = batch()
+    const id = nanoid()
+    const sashaRef = ref(users, `${id}-sasha`)
+    const tatiRef = ref(users, `${id}-tati`)
+    const edRef = ref(users, `${id}-ed`)
+    set(sashaRef, { name: 'Sasha' })
+    set(tatiRef, { name: 'Tati' })
+    set(edRef, { name: 'Ed' })
+    await commit()
+    const [sasha, tati, ed] = await Promise.all([
+      get(sashaRef),
+      get(tatiRef),
+      get(edRef)
+    ])
+    assert(sasha.data.name === 'Sasha')
+    assert(tati.data.name === 'Tati')
+    assert(ed.data.name === 'Ed')
+  })
+
+  it('allows updating', async () => {
+    const { update, commit } = batch()
+    const id = nanoid()
+    const sashaRef = ref(users, `${id}-sasha`)
+    const tatiRef = ref(users, `${id}-tati`)
+    const edRef = ref(users, `${id}-ed`)
+    await Promise.all([
+      set(sashaRef, { name: 'Sasha' }),
+      set(tatiRef, { name: 'Tati' }),
+      set(edRef, { name: 'Ed' })
+    ])
+    update(sashaRef, { name: 'Sasha Koss' })
+    update(tatiRef, { name: 'Tati Shepeleva' })
+    update(edRef, { name: 'Ed Tsech' })
+    await commit()
+    const [sasha, tati, ed] = await Promise.all([
+      get(sashaRef),
+      get(tatiRef),
+      get(edRef)
+    ])
+    assert(sasha.data.name === 'Sasha Koss')
+    assert(tati.data.name === 'Tati Shepeleva')
+    assert(ed.data.name === 'Ed Tsech')
+  })
+
+  it('allows clearing', async () => {
+    const { clear, commit } = batch()
+    const id = nanoid()
+    const sashaRef = ref(users, `${id}-sasha`)
+    const tatiRef = ref(users, `${id}-tati`)
+    const edRef = ref(users, `${id}-ed`)
+    await Promise.all([
+      set(sashaRef, { name: 'Sasha' }),
+      set(tatiRef, { name: 'Tati' }),
+      set(edRef, { name: 'Ed' })
+    ])
+    clear(sashaRef)
+    clear(tatiRef)
+    clear(edRef)
+    await commit()
+    const [sasha, tati, ed] = await Promise.all([
+      get(sashaRef),
+      get(tatiRef),
+      get(edRef)
+    ])
+    assert(!sasha)
+    assert(!tati)
+    assert(!ed)
+  })
+})
