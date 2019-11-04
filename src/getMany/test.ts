49c216f1ae7186a74dba51002d7c952c3d3070ae
Sasha Koss <koss@nocorp.me>
2019-11-04 08:41:58 +0800

    Rewrite a test to be compatible with Jasmine

--- src/getMany/test.ts
+++ src/getMany/test.ts
@@ -26,12 +26,12 @@ describe('getMany', () => {
 
   it('returns nothing when called with empty array', async () => {
     const list = await getMany(fruits, [])
-    expect(list).toHaveLength(0)
+    expect(list.length).toBe(0)
   })
 
   it('allows to get single doc by id', async () => {
     const fruitsFromDB = await getMany(fruits, ['apple'])
-    expect(fruitsFromDB).toHaveLength(1)
+    expect(fruitsFromDB.length).toBe(1)
     expect(fruitsFromDB[0].__type__).toBe('doc')
     expect(fruitsFromDB[0].data.color).toBe('green')
     expect(fruitsFromDB[0].ref.id).toBe('apple')
@@ -45,25 +45,27 @@ describe('getMany', () => {
       'banana',
       'orange'
     ])
-    expect(fruitsFromDB).toHaveLength(4)
+    expect(fruitsFromDB.length).toBe(4)
     expect(fruitsFromDB[0].ref.id).toBe('banana')
     expect(fruitsFromDB[1].ref.id).toBe('apple')
     expect(fruitsFromDB[2].ref.id).toBe('banana')
     expect(fruitsFromDB[3].ref.id).toBe('orange')
   })
 
-  it('throws an error when an id is missing', async () => {
-    const promise = getMany(fruits, ['nonexistant'])
-    await expect(promise).rejects.toThrowErrorMatchingInlineSnapshot(
-      `"Missing document with id nonexistant"`
-    )
-  })
+  it('throws an error when an id is missing', () =>
+    getMany(fruits, ['nonexistant'])
+      .then(() => {
+        throw new Error('The promise should be rejected')
+      })
+      .catch(err => {
+        expect(err.message).toBe('Missing document with id nonexistant')
+      }))
 
   it('allows to specify custom logic when a document is not found', async () => {
     const list = await getMany(fruits, ['nonexistant'], id => ({
       color: `${id} is missing but I filled it in`
     }))
-    expect(list).toHaveLength(1)
+    expect(list.length).toBe(1)
     expect(list[0].data.color).toBe('nonexistant is missing but I filled it in')
   })
 
@@ -73,6 +75,6 @@ describe('getMany', () => {
       ['apple', 'nonexistant', 'banana'],
       'ignore'
     )
-    expect(list).toHaveLength(2)
+    expect(list.length).toBe(2)
   })
 })
