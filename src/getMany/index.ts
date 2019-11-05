03f5a00a2610c50ed6c987038857606e675564b0
Thomas Toye <thomas@toye.io>
2019-11-05 01:34:34 +0100

    Add getMany that allows getting multiple documents by ids (#10)

---
+++ src/getMany/index.ts
@@ -0,0 +1,71 @@
+import firestore from '../adaptor'
+import { Collection } from '../collection'
+import { doc, Doc } from '../doc'
+import { ref } from '../ref'
+import { wrapData } from '../data'
+
+/**
+ * Retrieves multiple documents from a collection.
+ *
+ * You can specify a strategy to handle missing documents by passing the `onMissing` argument.
+ * By default, missing documents will throw an error. Other strategies:
+ *
+ *  * By providing `(id) => new MyModel(id, ...)`, you can provide a default value when a doc is missing
+ *  * By providing `'ignore'`, missing documents are ignore and will be removed from the result
+ *  * By providing `(id) => throw new CustomError(id)`, you can throw a a custom error
+ *
+ * ```ts
+ * import { getMany, collection } from 'typesaurus'
+ *
+ * type User = { name: string }
+ * const users = collection<User>('users')
+ *
+ * getMany(users, ['00sHm46UWKObv2W7XK9e', '00sHm46UWKObv2W7XK0d']).then(user => {
+ *   console.log(user)
+ *   //=> [ { __type__: 'doc', data: { name: 'Sasha' }, ... }, { __type__: 'doc', data: { name: 'Thomas' }, ... }]
+ * })
+ * ```
+ *
+ * @returns Promise to a list of found documents
+ */
+async function getMany<Model>(
+  collection: Collection<Model>,
+  ids: readonly string[],
+  onMissing: ((id: string) => Model) | 'ignore' = id => {
+    throw new Error(`Missing document with id ${id}`)
+  }
+): Promise<Doc<Model>[]> {
+  if (ids.length === 0) {
+    // Firestore#getAll doesn't like empty lists
+    return Promise.resolve([])
+  }
+
+  const firestoreSnaps = await firestore().getAll(
+    ...ids.map(id =>
+      firestore()
+        .collection(collection.path)
+        .doc(id)
+    )
+  )
+
+  return firestoreSnaps
+    .map(firestoreSnap => {
+      if (!firestoreSnap.exists) {
+        if (onMissing === 'ignore') {
+          return null
+        } else {
+          return doc(
+            ref(collection, firestoreSnap.id),
+            onMissing(firestoreSnap.id)
+          )
+        }
+      }
+
+      const firestoreData = firestoreSnap.data()
+      const data = firestoreData && (wrapData(firestoreData) as Model)
+      return doc(ref(collection, firestoreSnap.id), data)
+    })
+    .filter(doc => doc != null) as Doc<Model>[]
+}
+
+export default getMany
---
+++ src/getMany/test.ts
@@ -0,0 +1,80 @@
+import { collection } from '../collection'
+import set from '../set'
+import getMany from '.'
+import remove from '../remove'
+
+describe('getMany', () => {
+  type Fruit = { color: string }
+
+  const fruits = collection<Fruit>('fruits')
+
+  beforeAll(async () => {
+    await Promise.all([
+      set(fruits, 'apple', { color: 'green' }),
+      set(fruits, 'banana', { color: 'yellow' }),
+      set(fruits, 'orange', { color: 'orange' })
+    ])
+  })
+
+  afterAll(async () => {
+    await Promise.all([
+      remove(fruits, 'apple'),
+      remove(fruits, 'banana'),
+      remove(fruits, 'orange')
+    ])
+  })
+
+  it('returns nothing when called with empty array', async () => {
+    const list = await getMany(fruits, [])
+    expect(list.length).toBe(0)
+  })
+
+  it('allows to get single doc by id', async () => {
+    const fruitsFromDB = await getMany(fruits, ['apple'])
+    expect(fruitsFromDB.length).toBe(1)
+    expect(fruitsFromDB[0].__type__).toBe('doc')
+    expect(fruitsFromDB[0].data.color).toBe('green')
+    expect(fruitsFromDB[0].ref.id).toBe('apple')
+    expect(fruitsFromDB[0].ref.collection.path).toBe('fruits')
+  })
+
+  it('allows to get multiple docs by id', async () => {
+    const fruitsFromDB = await getMany(fruits, [
+      'banana',
+      'apple',
+      'banana',
+      'orange'
+    ])
+    expect(fruitsFromDB.length).toBe(4)
+    expect(fruitsFromDB[0].ref.id).toBe('banana')
+    expect(fruitsFromDB[1].ref.id).toBe('apple')
+    expect(fruitsFromDB[2].ref.id).toBe('banana')
+    expect(fruitsFromDB[3].ref.id).toBe('orange')
+  })
+
+  it('throws an error when an id is missing', () =>
+    getMany(fruits, ['nonexistant'])
+      .then(() => {
+        throw new Error('The promise should be rejected')
+      })
+      .catch(err => {
+        expect(err.message).toBe('Missing document with id nonexistant')
+      }))
+
+  it('allows to specify custom logic when a document is not found', async () => {
+    const list = await getMany(fruits, ['nonexistant'], id => ({
+      color: `${id} is missing but I filled it in`
+    }))
+    expect(list.length).toBe(1)
+    expect(list[0].data.color).toBe('nonexistant is missing but I filled it in')
+  })
+
+  it('allows to ignore missing documents', async () => {
+    const list = await getMany(
+      fruits,
+      ['apple', 'nonexistant', 'banana'],
+      'ignore'
+    )
+    expect(list.length).toBe(2)
+  })
+})
