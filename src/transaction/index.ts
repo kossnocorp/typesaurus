268730d7c61f93674f566720a822b9c7d5fa5f7a
Sasha Koss <koss@nocorp.me>
2020-01-01 22:15:07 +0400

    Restore accidentally removed update

--- src/transaction/index.ts
+++ src/transaction/index.ts
@@ -255,6 +255,43 @@ export function transaction(transactionFn: TransactionFunction): Promise<any> {
       return doc(ref(collection, id), data)
     }
 
+    async function update<Model>(
+      collectionOrRef: Collection<Model> | Ref<Model>,
+      idOrData: string | Field<Model>[] | ModelUpdate<Model>,
+      maybeData?: Field<Model>[] | ModelUpdate<Model>
+    ): Promise<void> {
+      let collection: Collection<Model>
+      let id: string
+      let data: Model
+
+      if (collectionOrRef.__type__ === 'collection') {
+        collection = collectionOrRef as Collection<Model>
+        id = idOrData as string
+        data = maybeData as Model
+      } else {
+        const ref = collectionOrRef as Ref<Model>
+        collection = ref.collection
+        id = ref.id
+        data = idOrData as Model
+      }
+
+      const firebaseDoc = firestore()
+        .collection(collection.path)
+        .doc(id)
+      const updateData = Array.isArray(data)
+        ? data.reduce(
+            (acc, { key, value }) => {
+              acc[Array.isArray(key) ? key.join('.') : key] = value
+              return acc
+            },
+            {} as { [key: string]: any }
+          )
+        : data
+      // ^ above
+      // TODO: Refactor code above because is all the same as in the regular update function
+      await t.update(firebaseDoc, unwrapData(updateData))
+    }
+
     async function remove<Model>(
       collectionOrRef: Collection<Model> | Ref<Model>,
       maybeId?: string
