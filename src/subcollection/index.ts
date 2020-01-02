2e1891628934d16dc48ddd49f8421bd7caae9119
Sasha Koss <koss@nocorp.me>
2020-01-02 14:04:18 +0400

    Add docs for nested subcollections

--- src/subcollection/index.ts
+++ src/subcollection/index.ts
@@ -14,12 +14,16 @@ export type NestedSubcollection<
   ParentIds extends Array<string>
 > = (ref: Ref<ParentModel> | ParentIds) => Collection<Model>
 
-function subcollection<Model, ParentModel>(
+export function subcollection<Model, ParentModel>(
   name: string,
   parentCollection: Collection<ParentModel>
 ): Subcollection<Model, ParentModel>
 
-function subcollection<Model, SubcollectionModel, SubcollectionParentModel>(
+export function subcollection<
+  Model,
+  SubcollectionModel,
+  SubcollectionParentModel
+>(
   name: string,
   parentSubcollection: Subcollection<
     SubcollectionModel,
@@ -27,7 +31,7 @@ function subcollection<Model, SubcollectionModel, SubcollectionParentModel>(
   >
 ): NestedSubcollection<Model, SubcollectionModel, [string, string]>
 
-function subcollection<
+export function subcollection<
   Model,
   SubcollectionModel,
   SubcollectionParentModel,
@@ -41,7 +45,7 @@ function subcollection<
   >
 ): NestedSubcollection<Model, SubcollectionModel, [string, string, string]>
 
-function subcollection<
+export function subcollection<
   Model,
   SubcollectionModel,
   SubcollectionParentModel,
@@ -59,7 +63,7 @@ function subcollection<
   [string, string, string, string]
 >
 
-function subcollection<
+export function subcollection<
   Model,
   SubcollectionModel,
   SubcollectionParentModel,
@@ -78,7 +82,7 @@ function subcollection<
 >
 
 /**
- * Creates a subcollection function which accepts parent document reference
+ * Creates a subcollection function which accepts parent document reference or id
  * and returns the subcollection trasnformed into a collection object.
  *
  * ```ts
@@ -92,16 +96,74 @@ function subcollection<
  * const sashasOrders = userOrders('00sHm46UWKObv2W7XK9e')
  * //=> { __type__: 'collection', path: 'users/00sHm46UWKObv2W7XK9e/orders' }
  * // Also accepts reference:
- * userOrders(ref(users, '00sHm46UWKObv2W7XK9e')))
+ * userOrders(ref(users, '00sHm46UWKObv2W7XK9e'))
  *
  * add(sashasOrders, { item: 'Snowboard boots' })
  * ```
  *
+ * The subcollection function can be passed as the parent collection to
+ * create nested subcollection:
+ *
+ * ```ts
+ * import { subcollection, collection, ref, add, Ref } from '.'
+ *
+ * type User = { name: string }
+ * type Post = { author: Ref<User>; text: string; date?: Date }
+ * type Comment = { author: Ref<User>; text: string; date?: Date }
+ * type Like = { author: Ref<User> }
+ *
+ * const users = collection<User>('users')
+ * const userPosts = subcollection<Post, User>('posts', users)
+ * // Generic types:
+ * // - Comment - the subcollection model
+ * // - Post - the parent model
+ * // - User - the grandparent model
+ * const postComments = subcollection<Comment, Post, User>('comments', userPosts)
+ * // Generic types:
+ * // - Like - the subcollection model
+ * // - Comment - the parent model
+ * // - Post - the grandparent model
+ * // - [string, string] - the ids type, a string for each level of nesting
+ * const commentLikes = subcollection<Like, Comment, Post, [string, string]>(
+ *   'likes',
+ *   postComments
+ * )
+ *
+ * // Using ids:
+ *
+ * const userId = '00sHm46UWKObv2W7XK9e'
+ * const postId = '2ZWn8t4w2F3LVz2azVCN'
+ * const commentId = 'UBHKjURqRZVpjvCwSKuO'
+ *
+ * userPosts(userId)
+ * //=> { __type__: 'collection', path: 'users/00sHm46UWKObv2W7XK9e/posts' }
+ *
+ * postComments([userId, postId])
+ * //=> { __type__: 'collection', path: 'users/00sHm46UWKObv2W7XK9e/posts/2ZWn8t4w2F3LVz2azVCN/comments' }
+ *
+ * commentLikes([userId, postId, commentId])
+ * //=> { __type__: 'collection', path: 'users/00sHm46UWKObv2W7XK9e/posts/2ZWn8t4w2F3LVz2azVCN/comments/UBHKjURqRZVpjvCwSKuO/likes' }
+ *
+ * // Or using refs:
+ *
+ * const user = ref(users, userId)
+ * userPosts(user)
+ * //=> { __type__: 'collection', path: 'users/00sHm46UWKObv2W7XK9e/posts' }
+ *
+ * const post = ref(userPosts(user))
+ * postComments(post)
+ * //=> { __type__: 'collection', path: 'users/00sHm46UWKObv2W7XK9e/posts/2ZWn8t4w2F3LVz2azVCN/comments' }
+ *
+ * const comment = ref(postComments(post))
+ * commentLikes(comment)
+ * //=> { __type__: 'collection', path: 'users/00sHm46UWKObv2W7XK9e/posts/2ZWn8t4w2F3LVz2azVCN/comments/UBHKjURqRZVpjvCwSKuO/likes' }
+ * ```
+ *
  * @param name - The subcollection name
- * @param parentCollection - The parent collection
- * @returns Function which accepts parent document
+ * @param parentCollection - The parent collection, subcollection or nested subcollection
+ * @returns Function which accepts parent document ref or id and returns collection object
  */
-function subcollection<Model, ParentModel>(
+export function subcollection<Model, ParentModel>(
   name: string,
   parentCollection:
     | Collection<ParentModel>
@@ -131,5 +193,3 @@ function subcollection<Model, ParentModel>(
     return collection<Model>(`${coll.path}/${id}/${name}`)
   }
 }
-
-export { subcollection }
