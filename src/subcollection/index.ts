b068f473fb24e33dd37fb6117f425ef649eb353b
Sasha Koss <koss@nocorp.me>
2020-01-02 07:53:31 +0400

    Nested subcollections prototype

--- src/subcollection/index.ts
+++ src/subcollection/index.ts
@@ -4,9 +4,78 @@ import { Collection, collection } from '../collection'
 /**
  * The subcollection function type.
  */
-export type Subcollection<RefModel, CollectionModel> = (
-  ref: Ref<RefModel> | string
-) => Collection<CollectionModel>
+export type Subcollection<Model, ParentModel> = (
+  ref: Ref<ParentModel> | string
+) => Collection<Model>
+
+export type NestedSubcollection<
+  Model,
+  ParentModel,
+  ParentIds extends Array<string>
+> = (ref: Ref<ParentModel> | ParentIds) => Collection<Model>
+
+function subcollection<Model, ParentModel>(
+  name: string,
+  parentCollection: Collection<ParentModel>
+): Subcollection<Model, ParentModel>
+
+function subcollection<Model, SubcollectionModel, SubcollectionParentModel>(
+  name: string,
+  parentSubcollection: Subcollection<
+    SubcollectionModel,
+    SubcollectionParentModel
+  >
+): NestedSubcollection<Model, SubcollectionModel, [string, string]>
+
+function subcollection<
+  Model,
+  SubcollectionModel,
+  SubcollectionParentModel,
+  SubcollectionIds extends [string, string]
+>(
+  name: string,
+  parentSubcollection: NestedSubcollection<
+    SubcollectionModel,
+    SubcollectionParentModel,
+    SubcollectionIds
+  >
+): NestedSubcollection<Model, SubcollectionModel, [string, string, string]>
+
+function subcollection<
+  Model,
+  SubcollectionModel,
+  SubcollectionParentModel,
+  SubcollectionIds extends [string, string, string]
+>(
+  name: string,
+  parentSubcollection: NestedSubcollection<
+    SubcollectionModel,
+    SubcollectionParentModel,
+    SubcollectionIds
+  >
+): NestedSubcollection<
+  Model,
+  SubcollectionModel,
+  [string, string, string, string]
+>
+
+function subcollection<
+  Model,
+  SubcollectionModel,
+  SubcollectionParentModel,
+  SubcollectionIds extends [string, string, string, string]
+>(
+  name: string,
+  parentSubcollection: NestedSubcollection<
+    SubcollectionModel,
+    SubcollectionParentModel,
+    SubcollectionIds
+  >
+): NestedSubcollection<
+  Model,
+  SubcollectionModel,
+  [string, string, string, string, string]
+>
 
 /**
  * Creates a subcollection function which accepts parent document reference
@@ -32,17 +101,35 @@ export type Subcollection<RefModel, CollectionModel> = (
  * @param parentCollection - The parent collection
  * @returns Function which accepts parent document
  */
-function subcollection<CollectionModel, RefModel>(
+function subcollection<Model, ParentModel>(
   name: string,
-  parentCollection: Collection<RefModel>
-): Subcollection<RefModel, CollectionModel> {
+  parentCollection:
+    | Collection<ParentModel>
+    | Subcollection<Model, ParentModel>
+    | NestedSubcollection<Model, ParentModel, string[]>
+): any {
   // TODO: Throw an exception when a collection has different name
-  return ref =>
-    collection<RefModel>(
-      `${parentCollection.path}/${
-        typeof ref === 'string' ? ref : ref.id
-      }/${name}`
-    )
+  return (ref: Ref<ParentModel> | string | string[]) => {
+    let id: string
+    let coll: Collection<any>
+
+    if (Array.isArray(ref)) {
+      const ids = ref
+      id = ids.pop() as string
+      coll =
+        typeof parentCollection === 'function'
+          ? (parentCollection as NestedSubcollection<any, any, string[]>)(ids)
+          : parentCollection
+    } else if (typeof ref === 'string') {
+      id = ref
+      coll = parentCollection as Collection<any>
+    } else {
+      id = ref.id
+      coll = ref.collection
+    }
+
+    return collection<Model>(`${coll.path}/${id}/${name}`)
+  }
 }
 
 export { subcollection }
--- src/subcollection/test.ts
+++ src/subcollection/test.ts
@@ -6,6 +6,8 @@ import { collection } from '../collection'
 describe('Subcollection', () => {
   type User = { name: string }
   type Post = { author: Ref<User>; text: string; date?: Date }
+  type Comment = { author: Ref<User>; text: string; date?: Date }
+  type Like = { author: Ref<User> }
   const users = collection<User>('users')
 
   describe('subcollection', () => {
@@ -23,6 +25,40 @@ describe('Subcollection', () => {
       assert.deepEqual(userPosts('42'), {
         __type__: 'collection',
         path: 'users/42/posts'
+      })
+    })
+
+    it('allows creating nested subcollections', () => {
+      const userPosts = subcollection<Post, User>('posts', users)
+      const postComments = subcollection<Comment, Post, User>(
+        'comments',
+        userPosts
+      )
+      const commentLikes = subcollection<Like, Comment, Post, [string, string]>(
+        'likes',
+        postComments
+      )
+
+      const user = ref(users, '42')
+      const post = ref(userPosts(user), '69')
+      const comment = ref(postComments(post), '13')
+
+      assert.deepEqual(postComments(post), {
+        __type__: 'collection',
+        path: 'users/42/posts/69/comments'
+      })
+      assert.deepEqual(postComments(['42', '69']), {
+        __type__: 'collection',
+        path: 'users/42/posts/69/comments'
+      })
+
+      assert.deepEqual(commentLikes(comment), {
+        __type__: 'collection',
+        path: 'users/42/posts/69/comments/13/likes'
+      })
+      assert.deepEqual(commentLikes(['42', '69', '13']), {
+        __type__: 'collection',
+        path: 'users/42/posts/69/comments/13/likes'
       })
     })
   })
