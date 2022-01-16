5383f370d6e06572841d16bf39afa22713d39f22
Henrique Bruno <henrique.bruno.fa@gmail.com>
2022-01-16 15:05:06 -0300

    Changed let to const, added comment.

--- src/settings.ts
+++ src/settings.ts
@@ -1,11 +1,22 @@
 export type TypesaurusSettings = {
+  /**
+   * If Typesaurus shall convert any `undefined` values in write operations to `null`.
+   *
+   * If `false`, the `undefined` behavior is handled by
+   * [Firestore `ignoreUndefinedProperties` setting](https://firebase.google.com/docs/reference/js/v8/firebase.firestore.Settings#ignoreundefinedproperties).
+   * You can change it via `firebase.firestore().settings({ignoreUndefinedProperties: value})`, in Node env.
+   * @default true
+   */
   undefinedToNull: boolean;
 }
 
-export let Settings: TypesaurusSettings = {
+export const Settings: TypesaurusSettings = {
   undefinedToNull: true
 }
 
+/** Allows changing settings of Typesaurus. */
 export function typesaurusSettings(settings: Partial<TypesaurusSettings>): void {
-  Settings = {...Settings, ...settings};
+  Object.assign(Settings, settings);
 }
+
+typesaurusSettings({})
