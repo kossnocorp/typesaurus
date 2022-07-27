5a91a612d9fea110f0fed1b5bedc885e8deeeac2
Sasha Koss <koss@nocorp.me>
2022-07-27 10:44:46 +0800

    remove tests

--- src/tests/remove.ts
+++ src/tests/remove.ts
@@ -1,27 +1,35 @@
-it.todo('TODO')
+import { schema } from '../adaptor'
 
-// import { get } from '../get'
-// import { add } from '../add'
-// import { remove } from '.'
-// import assert from 'assert'
-// import { collection } from '../collection'
+describe('remove', () => {
+  interface User {
+    name: string
+  }
 
-// describe('remove', () => {
-//   type User = { name: string }
-//   const users = collection<User>('users')
+  const db = schema(($) => ({
+    users: $.collection<User>()
+  }))
 
-//   it('removes document', async () => {
-//     const user = await add(users, { name: 'Sasha' })
-//     const { id } = user
-//     await remove(users, id)
-//     const userFromDB = await get(users, id)
-//     assert(userFromDB === null)
-//   })
+  it('removes document', async () => {
+    const user = await db.users.add({ name: 'Sasha' })
+    const { id } = user
+    await db.users.remove(id)
+    const userFromDB = await user.get()
+    expect(userFromDB).toBeNull()
+  })
 
-//   it('allows removing by ref', async () => {
-//     const user = await add(users, { name: 'Sasha' })
-//     await remove(user)
-//     const userFromDB = await get(users, user.id)
-//     assert(userFromDB === null)
-//   })
-// })
+  it('allows removing via ref', async () => {
+    const user = await db.users.add({ name: 'Sasha' })
+    await user.remove()
+    const userFromDB = await user.get()
+    expect(userFromDB).toBeNull()
+  })
+
+  it('allows removing via doc', async () => {
+    const user = await db.users.add({ name: 'Sasha' })
+    const userDoc = await user.get()
+    if (!userDoc) throw new Error('Document is not present')
+    await userDoc.remove()
+    const userFromDB = await user.get()
+    expect(userFromDB).toBeNull()
+  })
+})
