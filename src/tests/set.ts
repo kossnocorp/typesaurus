38cd05595e384bc85f6903e46e9974448d86e2ee
Sasha Koss <koss@nocorp.me>
2022-07-28 17:05:59 +0800

    set function

--- src/tests/set.ts
+++ src/tests/set.ts
@@ -1,147 +1,150 @@
-it.todo('TODO')
+import { nanoid } from 'nanoid'
+import { Typesaurus } from '..'
+import { schema } from '../adaptor'
 
-// import assert from 'assert'
-// import { nanoid } from 'nanoid'
-// import { set } from '.'
-// import { collection } from '../collection'
-// import { get } from '../get'
-// import { Ref, ref } from '../ref'
-// import { ServerDate } from '../types'
-// import { value } from '../value'
+describe('set', () => {
+  interface User {
+    name: string
+  }
 
-// describe('set', () => {
-//   type User = { name: string }
-//   type Post = { author: Ref<User>; text: string; date?: Date }
+  interface Post {
+    author: Typesaurus.Ref<User>
+    text: string
+    date?: Date
+  }
 
-//   const users = collection<User>('users')
-//   const posts = collection<Post>('post')
+  const db = schema(($) => ({
+    users: $.collection<User>(),
+    posts: $.collection<Post>()
+  }))
 
-//   it('sets a document', async () => {
-//     const id = nanoid()
-//     await set(users, id, { name: 'Sasha' })
-//     const user = await get(users, id)
-//     assert.deepEqual(user?.data, { name: 'Sasha' })
-//   })
+  it('sets a document', async () => {
+    const id = nanoid()
+    await db.users.set(id, { name: 'Sasha' })
+    const user = await db.users.get(id)
+    expect(user?.data).toEqual({ name: 'Sasha' })
+  })
 
-//   it('overwrites a document', async () => {
-//     const id = nanoid()
-//     await set(users, id, { name: 'Sasha' })
-//     await set(users, id, { name: 'Sasha Koss' })
-//     const user = await get(users, id)
-//     assert.deepEqual(user?.data, { name: 'Sasha Koss' })
-//   })
+  it('overwrites a document', async () => {
+    const id = nanoid()
+    await db.users.set(id, { name: 'Sasha' })
+    await db.users.set(id, { name: 'Sasha Koss' })
+    const user = await db.users.get(id)
+    expect(user?.data).toEqual({ name: 'Sasha Koss' })
+  })
 
-//   it('allows setting to refs', async () => {
-//     const id = nanoid()
-//     const userRef = ref(users, id)
-//     await set(userRef, { name: 'Sasha' })
-//     const user = await get(users, id)
-//     assert.deepEqual(user?.data, { name: 'Sasha' })
-//   })
+  it('allows setting to refs', async () => {
+    const id = nanoid()
+    const userRef = db.users.ref(id)
+    await userRef.set({ name: 'Sasha' })
+    const user = await userRef.get()
+    expect(user?.data).toEqual({ name: 'Sasha' })
+  })
 
-//   it('supports references', async () => {
-//     const userId = nanoid()
-//     const postId = nanoid()
-//     await set(users, userId, { name: 'Sasha' })
-//     await set(posts, postId, { author: ref(users, userId), text: 'Hello!' })
-//     const postFromDB = await get(posts, postId)
-//     const userFromDB =
-//       postFromDB && (await get(users, postFromDB?.data.author.id))
-//     assert.deepEqual(userFromDB?.data, { name: 'Sasha' })
-//   })
+  it('supports references', async () => {
+    const userId = nanoid()
+    const postId = nanoid()
+    await db.users.set(userId, { name: 'Sasha' })
+    await db.posts.set(postId, { author: db.users.ref(userId), text: 'Hello!' })
+    const postFromDB = await db.posts.get(postId)
+    const userFromDB = postFromDB && (await postFromDB?.data.author.get())
+    expect(userFromDB?.data).toEqual({ name: 'Sasha' })
+  })
 
-//   it('supports dates', async () => {
-//     const date = new Date()
-//     const userRef = ref(users, '42')
-//     const postId = nanoid()
-//     await set(posts, postId, { author: userRef, text: 'Hello!', date })
-//     const postFromDB = await get(posts, postId)
-//     assert(postFromDB?.data.date?.getTime() === date.getTime())
-//   })
+  it('supports dates', async () => {
+    const date = new Date()
+    const userRef = db.users.ref('42')
+    const postId = nanoid()
+    await db.posts.set(postId, { author: userRef, text: 'Hello!', date })
+    const postFromDB = await db.posts.get(postId)
+    expect(postFromDB?.data.date?.getTime()).toBe(date.getTime())
+  })
 
-//   describe('server dates', () => {
-//     const users = collection<User>('users')
+  describe('server dates', () => {
+    interface User {
+      name: string
+      createdAt: Typesaurus.ServerDate
+      updatedAt?: Typesaurus.ServerDate
+      birthday: Date
+    }
 
-//     interface User {
-//       name: string
-//       createdAt: ServerDate
-//       updatedAt?: ServerDate
-//       birthday: Date
-//     }
+    const db = schema(($) => ({
+      users: $.collection<User>()
+    }))
 
-//     it('supports server dates', async () => {
-//       const userId = nanoid()
-//       await set(users, userId, {
-//         name: 'Sasha',
-//         createdAt: value('serverDate'),
-//         updatedAt: value('serverDate'),
-//         birthday: new Date(1987, 1, 11)
-//       })
-//       const user = await get(users, userId)
-//       const now = Date.now()
-//       const returnedDate = user?.data.createdAt
-//       assert(returnedDate !== undefined)
-//       assert(returnedDate instanceof Date)
-//       assert(
-//         returnedDate!.getTime() < now && returnedDate!.getTime() > now - 10000
-//       )
-//     })
+    it('supports server dates', async () => {
+      const userId = nanoid()
+      await db.users.set(userId, ($) => ({
+        name: 'Sasha',
+        createdAt: $.serverDate(),
+        updatedAt: $.serverDate(),
+        birthday: new Date(1987, 1, 11)
+      }))
+      const user = await db.users.get(userId)
+      const now = Date.now()
+      const returnedDate = user?.data.createdAt
+      expect(returnedDate).not.toBeUndefined()
+      expect(returnedDate).toBeInstanceOf(Date)
+      expect(
+        returnedDate!.getTime() < now && returnedDate!.getTime() > now - 10000
+      ).toBe(true)
+    })
 
-//     it('allows to assert environment which allows setting dates', async () => {
-//       // TODO: Find a way to make the error show on fields like when assertEnvironment: 'web'
-//       // @ts-expect-error
-//       await set(users, nanoid(), {
-//         name: 'Sasha',
-//         createdAt: new Date(),
-//         updatedAt: new Date(),
-//         birthday: new Date(1987, 1, 11)
-//       })
+    //     it('allows to assert environment which allows setting dates', async () => {
+    //       // TODO: Find a way to make the error show on fields like when assertEnvironment: 'web'
+    //       // @ts-expect-error
+    //       await set(users, nanoid(), {
+    //         name: 'Sasha',
+    //         createdAt: new Date(),
+    //         updatedAt: new Date(),
+    //         birthday: new Date(1987, 1, 11)
+    //       })
 
-//       const nodeSet = () =>
-//         set(
-//           users,
-//           nanoid(),
-//           {
-//             name: 'Sasha',
-//             createdAt: new Date(),
-//             updatedAt: new Date(),
-//             birthday: new Date(1987, 1, 11)
-//           },
-//           { assertEnvironment: 'node' }
-//         )
+    //       const nodeSet = () =>
+    //         set(
+    //           users,
+    //           nanoid(),
+    //           {
+    //             name: 'Sasha',
+    //             createdAt: new Date(),
+    //             updatedAt: new Date(),
+    //             birthday: new Date(1987, 1, 11)
+    //           },
+    //           { assertEnvironment: 'node' }
+    //         )
 
-//       const webSet = () =>
-//         set(
-//           users,
-//           nanoid(),
-//           {
-//             name: 'Sasha',
-//             // @ts-expect-error
-//             createdAt: new Date(),
-//             // @ts-expect-error
-//             updatedAt: new Date(),
-//             birthday: new Date(1987, 1, 11)
-//           },
-//           { assertEnvironment: 'web' }
-//         )
+    //       const webSet = () =>
+    //         set(
+    //           users,
+    //           nanoid(),
+    //           {
+    //             name: 'Sasha',
+    //             // @ts-expect-error
+    //             createdAt: new Date(),
+    //             // @ts-expect-error
+    //             updatedAt: new Date(),
+    //             birthday: new Date(1987, 1, 11)
+    //           },
+    //           { assertEnvironment: 'web' }
+    //         )
 
-//       if (typeof window === 'undefined') {
-//         await nodeSet()
-//         try {
-//           await webSet()
-//           assert.fail('It should catch!')
-//         } catch (err) {
-//           assert(err.message === 'Expected web environment')
-//         }
-//       } else {
-//         await webSet()
-//         try {
-//           await nodeSet()
-//           assert.fail('It should catch!')
-//         } catch (err) {
-//           assert(err.message === 'Expected node environment')
-//         }
-//       }
-//     })
-//   })
-// })
+    //       if (typeof window === 'undefined') {
+    //         await nodeSet()
+    //         try {
+    //           await webSet()
+    //           assert.fail('It should catch!')
+    //         } catch (err) {
+    //           assert(err.message === 'Expected web environment')
+    //         }
+    //       } else {
+    //         await webSet()
+    //         try {
+    //           await nodeSet()
+    //           assert.fail('It should catch!')
+    //         } catch (err) {
+    //           assert(err.message === 'Expected node environment')
+    //         }
+    //       }
+    //     })
+  })
+})
