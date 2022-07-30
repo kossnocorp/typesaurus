0ebe5dda93d830993c9048cd8365580b69968418
Sasha Koss <koss@nocorp.me>
2022-07-30 14:17:22 +0800

    Enable more real-time tests

--- src/tests/get.ts
+++ src/tests/get.ts
@@ -1,26 +1,27 @@
+import sinon from 'sinon'
 import { Typesaurus } from '..'
 import { schema } from '../adaptor'
 
 describe('get', () => {
+  interface User {
+    name: string
+  }
+
+  interface Post {
+    author: Typesaurus.Ref<User>
+    text: string
+    date?: Date
+  }
+
+  interface Nope {}
+
+  const db = schema(($) => ({
+    users: $.collection<User>(),
+    posts: $.collection<Post>(),
+    nope: $.collection<Nope>()
+  }))
+
   describe('promise', () => {
-    interface User {
-      name: string
-    }
-
-    interface Post {
-      author: Typesaurus.Ref<User>
-      text: string
-      date?: Date
-    }
-
-    interface Nope {}
-
-    const db = schema(($) => ({
-      users: $.collection<User>(),
-      posts: $.collection<Post>(),
-      nope: $.collection<Nope>()
-    }))
-
     it('returns nothing if document is not present', async () => {
       const nothing = await db.nope.get('nah')
       expect(nothing).toBe(null)
@@ -58,105 +59,106 @@ describe('get', () => {
   })
 
   describe('subscription', () => {
-    // type User = { name: string }
-    // type Post = { author: Ref<User>; text: string; date?: Date }
-    // const users = collection<User>('users')
-    // const posts = collection<Post>('post')
-    // let off: (() => void) | undefined
-    // afterEach(() => {
-    //   off && off()
-    //   off = undefined
-    // })
-    // it('returns nothing if document is not present', (done) => {
-    //   off = onGet(collection('nope'), 'nah', (nothing) => {
-    //     assert(nothing === null)
-    //     done()
-    //   })
-    // })
-    // it('allows to get by ref', async () => {
-    //   const user = await add(users, { name: 'Sasha' })
-    //   return new Promise((resolve) => {
-    //     off = onGet(user, (userFromDB) => {
-    //       assert.deepEqual(userFromDB?.data, { name: 'Sasha' })
-    //       resolve(void 0)
-    //     })
-    //   })
-    // })
-    // it('expands references', async () => {
-    //   const user = await add(users, { name: 'Sasha' })
-    //   const post = await add(posts, {
-    //     author: user,
-    //     text: 'Hello!'
-    //   })
-    //   return new Promise((resolve) => {
-    //     off = onGet(posts, post.id, async (postFromDB) => {
-    //       assert(postFromDB?.data.author.__type__ === 'ref')
-    //       const userFromDB =
-    //         postFromDB && (await get(users, postFromDB.data.author.id))
-    //       assert.deepEqual(userFromDB?.data, { name: 'Sasha' })
-    //       resolve(void 0)
-    //     })
-    //   })
-    // })
-    // it('expands dates', async () => {
-    //   const date = new Date()
-    //   const userRef = ref(users, '42')
-    //   const post = await add(posts, {
-    //     author: userRef,
-    //     text: 'Hello!',
-    //     date
-    //   })
-    //   return new Promise((resolve) => {
-    //     off = onGet(posts, post.id, (postFromDB) => {
-    //       assert(postFromDB?.data.date?.getTime() === date.getTime())
-    //       resolve(void 0)
-    //     })
-    //   })
-    // })
-    // describe('real-time', () => {
-    //   it('subscribes to updates', async () => {
-    //     const spy = sinon.spy()
-    //     const user = await add(users, { name: 'Sasha' })
-    //     return new Promise((resolve) => {
-    //       off = onGet(user, async (doc) => {
-    //         const name = doc?.data.name
-    //         spy(name)
-    //         if (name === 'Sasha') {
-    //           await update(user, { name: 'Sasha Koss' })
-    //         } else if (name === 'Sasha Koss') {
-    //           assert(spy.calledWith('Sasha'))
-    //           assert(spy.calledWith('Sasha Koss'))
-    //           resolve(void 0)
-    //         }
-    //       })
-    //     })
-    //   })
-    //   // TODO: WTF browser Firebase returns elements gradually unlike Node.js version.
-    //   if (typeof window === undefined) {
-    //     it('returns function that unsubscribes from the updates', () => {
-    //       return new Promise(async (resolve) => {
-    //         const spy = sinon.spy()
-    //         const user = await add(users, { name: 'Sasha' })
-    //         const on = () => {
-    //           off = onGet(user, (doc) => {
-    //             const name = doc?.data.name
-    //             spy(name)
-    //             if (name === 'Sasha Koss') {
-    //               off?.()
-    //               assert(spy.neverCalledWith('Sashka'))
-    //               assert(spy.calledWith('Sasha Koss'))
-    //               resolve(void 0)
-    //             }
-    //           })
-    //         }
-    //         on()
-    //         off?.()
-    //         await update(user, { name: 'Sashka' })
-    //         await update(user, { name: 'Sasha Koss' })
-    //         on()
-    //       })
-    //     })
-    //   }
-    // })
+    let off: (() => void) | undefined
+    afterEach(() => {
+      off && off()
+      off = undefined
+    })
+
+    it('returns nothing if document is not present', () =>
+      new Promise((done) => {
+        off = db.nope.get('nah').on((nothing) => {
+          expect(nothing).toBeNull()
+          done(void 0)
+        })
+      }))
+
+    it('allows to get by ref', async () => {
+      const user = await db.users.add({ name: 'Sasha' })
+      return new Promise((resolve) => {
+        off = user.get().on((userFromDB) => {
+          expect(userFromDB?.data).toEqual({ name: 'Sasha' })
+          resolve(void 0)
+        })
+      })
+    })
+
+    it('expands references', async () => {
+      const user = await db.users.add({ name: 'Sasha' })
+      const post = await db.posts.add({
+        author: user,
+        text: 'Hello!'
+      })
+      return new Promise((resolve) => {
+        off = db.posts.get(post.id).on(async (postFromDB) => {
+          const userFromDB = await postFromDB?.data.author.get()
+          expect(userFromDB?.data).toEqual({ name: 'Sasha' })
+          resolve(void 0)
+        })
+      })
+    })
+
+    it('expands dates', async () => {
+      const date = new Date()
+      const userRef = db.users.ref('42')
+      const post = await db.posts.add({
+        author: userRef,
+        text: 'Hello!',
+        date
+      })
+
+      return new Promise((resolve) => {
+        off = db.posts.get(post.id).on((postFromDB) => {
+          expect(postFromDB?.data.date?.getTime()).toBe(date.getTime())
+          resolve(void 0)
+        })
+      })
+    })
+
+    describe('real-time', () => {
+      it('subscribes to updates', async () => {
+        const spy = sinon.spy()
+        const user = await db.users.add({ name: 'Sasha' })
+        return new Promise((resolve) => {
+          off = user.get().on(async (doc) => {
+            const name = doc?.data.name
+            spy(name)
+            if (name === 'Sasha') {
+              await user.update({ name: 'Sasha Koss' })
+            } else if (name === 'Sasha Koss') {
+              expect(spy.calledWith('Sasha')).toBe(true)
+              expect(spy.calledWith('Sasha Koss')).toBe(true)
+              resolve(void 0)
+            }
+          })
+        })
+      })
+
+      //   // TODO: WTF browser Firebase returns elements gradually unlike Node.js version.
+      //   if (typeof window === undefined) {
+      it('returns function that unsubscribes from the updates', () =>
+        new Promise(async (resolve) => {
+          const spy = sinon.spy()
+          const user = await db.users.add({ name: 'Sasha' })
+          const on = () => {
+            off = user.get().on((doc) => {
+              const name = doc?.data.name
+              spy(name)
+              if (name === 'Sasha Koss') {
+                off?.()
+                expect(spy.neverCalledWith('Sashka')).toBe(true)
+                expect(spy.calledWith('Sasha Koss'))
+                resolve(void 0)
+              }
+            })
+          }
+          on()
+          off?.()
+          await user.update({ name: 'Sashka' })
+          await user.update({ name: 'Sasha Koss' })
+          on()
+        }))
+      // }
+    })
   })
 })
