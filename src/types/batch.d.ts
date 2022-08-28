ebd5dd329db2dea8007e507c5cae981c5e3148a5
Sasha Koss <koss@nocorp.me>
2022-08-28 21:33:44 +0800

    Simplify the imported namespaces

--- src/types/batch.d.ts
+++ src/types/batch.d.ts
@@ -1,59 +1,49 @@
-import type { TypesaurusCore } from './core'
-import type { TypesaurusUpdate } from './update'
+import type { TypesaurusCore as Core } from './core'
+import type { TypesaurusUpdate as Update } from './update'
 
 export namespace TypesaurusBatch {
   export interface Function {
     <
-      DB extends TypesaurusCore.DB<any>,
-      Environment extends
-        | TypesaurusCore.RuntimeEnvironment
-        | undefined = undefined
+      DB extends Core.DB<any>,
+      Environment extends Core.RuntimeEnvironment | undefined = undefined
     >(
       db: DB,
-      options?: TypesaurusCore.OperationOptions<Environment>
+      options?: Core.OperationOptions<Environment>
     ): RootDB<DB, Environment>
   }
 
   export type RootDB<
-    DB extends TypesaurusCore.DB<any>,
-    Environment extends
-      | TypesaurusCore.RuntimeEnvironment
-      | undefined = undefined
+    DB extends Core.DB<any>,
+    Environment extends Core.RuntimeEnvironment | undefined = undefined
   > = BatchDB<DB, Environment> & {
     (): Promise<void>
   }
 
   export type BatchDB<
-    DB extends TypesaurusCore.DB<any>,
-    Environment extends
-      | TypesaurusCore.RuntimeEnvironment
-      | undefined = undefined
+    DB extends Core.DB<any>,
+    Environment extends Core.RuntimeEnvironment | undefined = undefined
   > = {
-    [Path in keyof DB]: DB[Path] extends TypesaurusCore.NestedRichCollection<
+    [Path in keyof DB]: DB[Path] extends Core.NestedRichCollection<
       infer Model,
       infer NestedDB
     >
       ? NestedCollection<Model, BatchDB<NestedDB, Environment>, Environment>
-      : DB[Path] extends TypesaurusCore.RichCollection<infer ModelPair>
+      : DB[Path] extends Core.RichCollection<infer ModelPair>
       ? Collection<ModelPair, Environment>
       : never
   }
 
   export type AnyCollection<
-    ModelPair extends TypesaurusCore.ModelIdPair,
-    Environment extends
-      | TypesaurusCore.RuntimeEnvironment
-      | undefined = undefined
+    ModelPair extends Core.ModelIdPair,
+    Environment extends Core.RuntimeEnvironment | undefined = undefined
   > =
     | Collection<ModelPair, Environment>
     | NestedCollection<ModelPair, Schema<Environment>, Environment>
 
   export interface NestedCollection<
-    ModelPair extends TypesaurusCore.ModelIdPair,
+    ModelPair extends Core.ModelIdPair,
     NestedSchema extends Schema<Environment>,
-    Environment extends
-      | TypesaurusCore.RuntimeEnvironment
-      | undefined = undefined
+    Environment extends Core.RuntimeEnvironment | undefined = undefined
   > extends Collection<ModelPair, Environment> {
     (id: ModelPair[1] /* Id */): NestedSchema
   }
@@ -62,39 +52,32 @@ export namespace TypesaurusBatch {
    *
    */
   export interface Collection<
-    ModelPair extends TypesaurusCore.ModelIdPair,
-    Environment extends
-      | TypesaurusCore.RuntimeEnvironment
-      | undefined = undefined
-  > extends TypesaurusCore.PlainCollection<ModelPair[0] /* Model */> {
+    ModelPair extends Core.ModelIdPair,
+    Environment extends Core.RuntimeEnvironment | undefined = undefined
+  > extends Core.PlainCollection<ModelPair[0] /* Model */> {
     /** The Firestore path */
     path: string
 
     set(
       id: ModelPair[1] /* Id */,
-      data: TypesaurusCore.WriteModelArg<ModelPair[0] /* Model */, Environment>
+      data: Core.WriteModelArg<ModelPair[0] /* Model */, Environment>
     ): void
 
     upset(
       id: ModelPair[1] /* Id */,
-      data: TypesaurusCore.WriteModelArg<ModelPair[0] /* Model */, Environment>
+      data: Core.WriteModelArg<ModelPair[0] /* Model */, Environment>
     ): void
 
     update(
       id: ModelPair[1] /* Id */,
-      data: TypesaurusUpdate.UpdateModelArg<
-        ModelPair[0] /* Model */,
-        Environment
-      >
+      data: Update.UpdateModelArg<ModelPair[0] /* Model */, Environment>
     ): void
 
     remove(id: ModelPair[1] /* Id */): void
   }
 
   export interface Schema<
-    Environment extends
-      | TypesaurusCore.RuntimeEnvironment
-      | undefined = undefined
+    Environment extends Core.RuntimeEnvironment | undefined = undefined
   > {
     [CollectionPath: string]: AnyCollection<any, Environment>
   }
